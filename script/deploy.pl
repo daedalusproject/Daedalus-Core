9bbc78fe2f69f907fbd98b0cfd9ef309e61b18e1
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-05-03 18:49:54 +0200

    Create script to deploy Daedalus::Core Realms database

--- script/deploy.pl
+++ script/deploy.pl
@@ -7,4 +7,21 @@ use warnings;
 use FindBin qw($Bin);
 use lib "$Bin/../lib";
 
-use DaedalusCore::Schema;
+use Daedalus::Core::Schema::CoreRealms;
+
+## Collect the database type, name, user, and password
+## from the command line:
+my $newdb_type = shift;
+my $newdb_name = shift;
+my $newdb_user = shift;
+my $newdb_pass = shift;
+
+## Create the schema object using the database connection info from above:
+
+my $schema = Daedalus::Core::Schema::CoreRealms->connect(
+    "dbi:${newdb_type}:${newdb_name}",
+    $newdb_user, $newdb_pass );
+
+## Call the deploy method on the schema object:
+$schema->deploy();
+
