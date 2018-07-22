2a0f18ec9d27ecb77889129c29a7b44ea37e4c81
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-07-22 17:44:55 +0000

    Add empty Daedalus::Messages::Manager

---
+++ lib/Daedalus/Messages/Manager.pm
@@ -0,0 +1,34 @@
+package Daedalus::Messages::Manager;
+
+=pod
+=encoding UTF-8
+=head1 NAME
+
+Daedalus::Messages::Manager
+
+=cut
+
+use strict;
+use warnings;
+use Daedalus::Hermes;
+
+use Data::Dumper;
+
+use namespace::clean -except => 'meta';
+
+=head1 NAME
+
+Daedalus::Messages::Manager
+
+=cut
+
+=head1 DESCRIPTION
+
+Daedalus Messages Manager
+
+=head1 METHODS
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+1;
