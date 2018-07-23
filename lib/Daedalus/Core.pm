d22840b039a0ac833be65add7a7093c2acb1ad6a
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-07-23 05:17:39 +0000

    Include hermes config inside Daedalus::Core config files

--- lib/Daedalus/Core.pm
+++ lib/Daedalus/Core.pm
@@ -4,18 +4,6 @@ use namespace::autoclean;
 
 use Catalyst::Runtime 5.80;
 
-# Set flags and add plugins for the application.
-#
-# Note that ORDERING IS IMPORTANT here as plugins are initialized in order,
-# therefore you almost certainly want to keep ConfigLoader at the head of the
-# list if you're using it.
-#
-#         -Debug: activates the debug mode for very useful log messages
-#   ConfigLoader: will load the configuration from a Config::General file in the
-#                 application's home directory
-# Static::Simple: will serve static files from the application's root
-#                 directory
-
 use Catalyst qw/
   -Debug
   ConfigLoader::Multi
@@ -39,6 +27,12 @@ if ( $ENV{APP_TEST} ) {
     __PACKAGE__->config( 'Plugin::ConfigLoader' =>
           { file => __PACKAGE__->path_to('t/lib/daedalus_core_testing.conf') }
     );
+    __PACKAGE__->config( 'Plugin::ConfigLoader' =>
+          { file => __PACKAGE__->path_to('t/lib/conf') } );
+}
+else {
+    __PACKAGE__->config(
+        'Plugin::ConfigLoader' => { file => __PACKAGE__->path_to('conf') } );
 }
 
 __PACKAGE__->config(
