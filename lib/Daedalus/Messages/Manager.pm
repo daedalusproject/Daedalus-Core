c6ecdde68e4efd76dc7033a657184ac4e1f1eaf1
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-07-23 18:17:09 +0000

    Undef all

--- lib/Daedalus/Messages/Manager.pm
+++ lib/Daedalus/Messages/Manager.pm
@@ -59,7 +59,8 @@ sub notify_new_user {
         { queue => 'daedalus_core_notifications', message => "test" } );
 
     undef $hermes;
+    undef $HERMES;
 }
 
-__PACKAGE__->meta->make_immutable;
+#__PACKAGE__->meta->make_immutable;
 1;
